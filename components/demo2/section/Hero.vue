812b1333aa5f67a29112a2c9b70d6ea7bc8b2377
Moonmangit <81956440+moonmangit@users.noreply.github.com>
2024-12-13 17:55:27 +0700

    refactor: Improve responsiveness of Hero and Nav components

--- components/demo2/section/Hero.vue
+++ components/demo2/section/Hero.vue
@@ -1,10 +1,17 @@
 <template>
-  <section class="h-[90dvh] duration-300 p-4 container mx-auto">
-    <div class="hero relative w-full h-full">
+  <section
+    ref="thisEl"
+    class="this h-[90dvh] duration-300 p-4 container mx-auto opening-clip-path-animate"
+  >
+    <div class="hero relative w-full h-full animate__animated animate__fadeIn">
       <div class="relative w-full h-full rounded-3xl overflow-hidden">
         <img
           src="https://images.unsplash.com/photo-1615630859219-0459703c34e6?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          class="w-full h-full object-cover"
+          class="w-full h-full object-cover absolute inset-0"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1615630859219-0459703c34e6?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          class="w-full h-full object-cover opacity-0 pointer-events-none"
           alt=""
         />
         <div class="absolute inset-0 bg-gradient-to-tr from-gray-800/90"></div>
@@ -13,17 +20,43 @@
       <section
         class="absolute inset-y-0 left-0 text-white flex flex-col justify-start lg:justify-center p-8 px-8 lg:px-10"
       >
-        <p class="font-light">1# Energy provider in the world</p>
-        <h1 class="text-3xl md:text-5xl lg:text-7xl my-5 md:my-10 line-clamp-2">
-          <b class="text-gradient">New Energy</b> <br />
-          for the Future
+        <p
+          class="font-light animate__animated animate__fadeInUp animate__delay-1s"
+          :style="{
+            animationDelay: '0.5s',
+          }"
+        >
+          1# Energy provider in the world
+        </p>
+        <h1
+          class="text-3xl md:text-5xl lg:text-7xl my-5 md:my-10 line-clamp-2 animate__animated animate__fadeIn"
+        >
+          <div class="relative">
+            <div
+              v-for="i in 2"
+              :key="i"
+              :class="{
+                absolute: i === 1,
+                'opacity-0 pointer-events-none': i === 2,
+              }"
+              class="animate__animated animate__fadeInLeft"
+            >
+              <b class="text-gradient"> New Energy </b>
+              <br />
+              <span>for the Future</span>
+            </div>
+          </div>
         </h1>
         <ul class="flex gap-x-10 gap-y-5 whitespace-nowrap flex-wrap text-sm">
-          <li class="flex items-center gap-x-2 border-b border-white pb-2">
-            Get in touch <Icon name="mdi:arrow-top-right" />
-          </li>
-          <li class="flex items-center gap-x-2 border-b border-white pb-2">
-            Our services <Icon name="mdi:arrow-top-right" />
+          <li
+            v-for="(label, i) in ['Get in touch', 'Our services']"
+            :key="i"
+            class="flex items-center gap-x-2 border-b border-white pb-2 animate__animated animate__fadeInUp"
+            :style="{
+              animationDelay: `${0.5 + i * 0.3}s`,
+            }"
+          >
+            {{ label }} <Icon name="mdi:arrow-top-right" />
           </li>
         </ul>
       </section>
@@ -69,4 +102,16 @@
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style scoped>
+.opening-clip-path-animate {
+  animation: opening-clip-path-animate 1.5s forwards;
+}
+@keyframes opening-clip-path-animate {
+  0% {
+    clip-path: circle(0% at 0% 50%);
+  }
+  100% {
+    clip-path: circle(100% at 50% 50%);
+  }
+}
+</style>
--- components/demo2/section/Nav.vue
+++ components/demo2/section/Nav.vue
@@ -4,8 +4,17 @@
   >
     <!-- logo -->
     <section class="flex items-center gap-x-1 text-xl">
-      <Icon name="fa6-brands:hive" class="text-[var(--p-green)]" />
-      <h1 class="font-bold">Xurya</h1>
+      <Icon
+        name="fa6-brands:hive"
+        class="text-[var(--p-green)] animate__animated animate__fadeInDown"
+        :style="{ animationDelay: `${BASE_ANIMATE_DELAY_SEC}s` }"
+      />
+      <h1
+        class="font-bold animate__animated animate__fadeInRight"
+        :style="{ animationDelay: `${BASE_ANIMATE_DELAY_SEC}s` }"
+      >
+        Xurya
+      </h1>
     </section>
     <!-- links -->
     <section class="hidden lg:block">
@@ -19,7 +28,12 @@
             'contact',
           ]"
           :key="idx"
-          class="text-[rgb(var(--p-black-rgb),0.8)]"
+          class="text-[rgb(var(--p-black-rgb),0.8)] animate__animated animate__fadeInUp"
+          :style="{
+            animationDelay: `${
+              BASE_ANIMATE_DELAY_SEC + Math.abs(3 - (idx + 1)) * 0.1
+            }s`,
+          }"
         >
           {{ link }}
         </li>
@@ -27,18 +41,31 @@
     </section>
     <!-- actions -->
     <section class="hidden lg:flex gap-x-2">
-      <button class="btn btn--white-border">Log in</button>
-      <button class="btn btn--green">Get in touch</button>
+      <button
+        class="btn btn--white-border animate__animated animate__fadeInDown"
+        :style="{ animationDelay: `${BASE_ANIMATE_DELAY_SEC}s` }"
+      >
+        Log in
+      </button>
+      <button
+        class="btn btn--green animate__animated animate__fadeInDown"
+        :style="{ animationDelay: `${BASE_ANIMATE_DELAY_SEC + 0.3}s` }"
+      >
+        Get in touch
+      </button>
     </section>
     <!-- mobile nav toggler  -->
     <button
-      class="text-2xl flex lg:hidden text-[rgba(var(--p-black-rgb),0.6)] p-2 rounded-xl"
+      class="text-2xl flex lg:hidden text-[rgba(var(--p-black-rgb),0.6)] p-2 rounded-xl animate__animated animate__fadeInLeft"
+      :style="{ animationDelay: `${BASE_ANIMATE_DELAY_SEC}s` }"
     >
       <Icon name="mdi:menu" />
     </button>
   </nav>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const BASE_ANIMATE_DELAY_SEC = 0.3;
+</script>
 
 <style></style>
